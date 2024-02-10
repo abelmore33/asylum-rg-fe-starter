d7c24fd9e9f6e74988be10468afcad02d4e5a64d
Aaron Belmore <belmoreaaron9@gmail.com>
2024-02-09 20:18:19 -0500

    Added data to the bottom container working on styling

--- src/components/pages/Landing/RenderLandingPage.jsx
+++ src/components/pages/Landing/RenderLandingPage.jsx
@@ -74,7 +74,32 @@ function RenderLandingPage(props) {
       </div>
       <div>
         {/* Bottom Section: Add code here for the graphs section for your first ticket */}
-        {/* <div className="bottom-section">*/}
+        <div className="bottom-section">
+          <h1>Systemic Disparity Insights</h1>
+          <div className="first-data-point-container">
+            <h2>36%</h2>
+            <p>
+              By the end of the Trump administration, the average asylum office
+              grant rate had fallen <strong>36 percent</strong> from an average
+              of 44% in fiscal year 2016 to 28 percent in ficalyear 2020.
+            </p>
+          </div>
+          <div className="second-data-point-container">
+            <h2>5%</h2>
+            <p>
+              The New York asylum office grant rate dropped to{' '}
+              <strong>5 perent</strong> in fiscal year 2020.
+            </p>
+          </div>
+          <div className="third-data-point-container">
+            <h2>6x Lower</h2>
+            <p>
+              Between fiscal year 2017 and 2020, the New York asylum office's
+              average grant rate was <strong>six times lower</strong> than the
+              San Francisco asylum office.
+            </p>
+          </div>
+        </div>
         <p onClick={() => scrollToTop()} className="back-to-top">
           Back To Top ^
         </p>
